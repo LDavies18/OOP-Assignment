82b74afd1a720d76c32931147bddf4aa76ae2172
LDavies18 <125997438+LDavies18@users.noreply.github.com>
2023-03-04 23:23:45 +0000

    Card Class First Attempt

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -14,30 +14,56 @@ namespace CMP1903M_A01_2223
         //The 'set' methods for these properties could have some validation
         //For one card to then be stored in a pack.
 
+
         //hides card value from being accesed from ouside class
 
-        private int card_value;
-        //Random generation?
-    
-        public int Value
-        {
-            get { return card_value; }
-            set { card_value = value; }
-        }
-       
-       //hides suit from being accessed from outside the class
+        private int card_num;
         private int card_suit;
+        private string gen_card;
+        public string str_suit;
+        public string str_num;
 
 
-        public int Suit
+        public int num
+        {
+            get { return card_num; }
+            set
+            {
+                Random num_rnd = new Random();
+                card_num = (num_rnd.Next(13);
+            }
+
+
+        }
+
+        public int suit
         {
             get { return card_suit; }
-            set { card_suit = value; }
+            set 
+            { 
+                Random suit_rnd = new Random();
+                card_suit = (suit_rnd.Next(4);
+            }
+
         }
 
-        static void Main(string[] args)
+        public string Gen_Card
         {
-          
+            get { return gen_card; }
+            set 
+            {
+                str_suit = suit.ToString();
+                str_num = num.ToString();   
+                gen_card = "suit" + str_suit + "value" + str_num;
+     
+            }
         }
     }
+
+
 }
+       
+     
+        
+    
+
